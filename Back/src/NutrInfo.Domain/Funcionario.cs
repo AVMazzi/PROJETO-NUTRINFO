9922f86f1a02d3967e31f689d85cf4601c825409
avmazzi03 <avmazzi03@gmail.com>
2024-05-23 00:41:21 -0300

    04 CORRECÃO EM PERMISSOES

--- Back/src/NutrInfo.Domain/Funcionario.cs
+++ Back/src/NutrInfo.Domain/Funcionario.cs
@@ -8,15 +8,15 @@ namespace NutrInfo.Domain
     public class Funcionario
     {
         public int ID { get; set; }
-public string Nome { get; set; }
-public DateTime DataNasc  { get; set; }
-public DateTime DataAdmissao { get; set; }
-public DateTime DataDemissão { get; set; }
-public string CPF { get; set; }
-public string RG { get; set; }
-public string Foto { get; set; }
-public string EstadoCivil { get; set; }
-public bool Ativo { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public DateTime DataDemissão { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Foto { get; set; }
+        public string EstadoCivil { get; set; }
+        public bool Ativo { get; set; }
 
     }
 }
